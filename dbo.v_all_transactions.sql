2b3fff68e80b3b7a31ba82f2f1a6576c1ab58eca
esoares2 <55720669+esoares2@users.noreply.github.com>
2019-11-04 22:16:24 -0700

    Add files via upload

---
+++ dbo.v_all_transactions.sql
@@ -0,0 +1,46 @@
+/********************************************************************************************
+NAME: [dbo].[v_all_transactions]
+PURPOSE: Create the view [dbo].[v_all_transactions]
+
+SUPPORT: Eloisa Soares
+	    esoares2@ldsbc.edu
+	    eloisa_mariano@yahoo.com.br
+
+MODIFICATION LOG:
+Ver   Date        Author    Description
+----  ----------  -------   -----------------------------------------------------------------
+1.0   11/04/2019  ESOARES   1. Built this script to create the view [dbo].[v_all_transactions].
+
+RUNTIME: 
+1 min
+
+NOTES: 
+(...)
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+
+********************************************************************************************/
+
+DROP VIEW IF EXISTS [dbo].[v_all_transactions];
+GO
+CREATE VIEW v_all_transactions
+AS
+(
+    SELECT tf.[transaction_date], 
+           tf.[transaction_time], 
+           tf.[branch_id], 
+           tf.[acct_id], 
+           tf.[transaction_type_id], 
+           ttd.[transaction_type_code], 
+           ttd.[transaction_type_desc], 
+           ttd.[transaction_fee_prct], 
+           ttd.[cur_cust_req_ind], 
+           tf.[transaction_amt], 
+           tf.[transaction_fee_amt]
+    FROM [dbo].[t_transaction_fact] AS tf
+         JOIN [dbo].[t_transaction_type_dim] AS ttd ON tf.transaction_type_id = ttd.transaction_type_id
+);
