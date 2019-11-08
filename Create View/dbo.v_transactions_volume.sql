2c880428b9e4b60036235abf8d8ee1f1f79268bd
esoares2 <55720669+esoares2@users.noreply.github.com>
2019-11-07 18:45:18 -0700

    Add files via upload

--- Create View/dbo.v_transactions_volume.sql
+++ Create View/dbo.v_transactions_volume.sql
@@ -32,11 +32,11 @@ AS
 (
     SELECT bd.[branch_code], 
            ttd.[transaction_type_desc], 
-           DATEPART(year, tf.[transaction_date]) AS date, 
            COUNT(tf.[transaction_date]) AS number_of_transactions
     FROM [dbo].[t_transaction_fact] AS tf
          LEFT JOIN [dbo].[t_branch_dim] AS bd ON tf.branch_id = bd.branch_id
          JOIN [dbo].[t_transaction_type_dim] AS ttd ON tf.transaction_type_id = ttd.transaction_type_id
+	WHERE DATEPART(year, tf.[transaction_date]) = 2019
     GROUP BY bd.[branch_code], 
              ttd.[transaction_type_desc], 
              DATEPART(year, tf.[transaction_date])
